5c40949e90940831fe388708b24b1f007e81a296
tut173311 <t173311@edu.tut.ac.jp>
2018-02-01 10:57:10 +0900

    e02011057

--- s4/b173311/Frequencer.java
+++ s4/b173311/Frequencer.java
@@ -15,6 +15,7 @@ interface FrequencerInterface {     // This interface provides the design for fr
 */
 
 /*
+//suffixarray無しバージョン
 public class Frequencer implements FrequencerInterface{
     // Code to Test, *warning: This code  contains intentional problem*
     byte [] myTarget;
@@ -81,9 +82,11 @@ public class Frequencer implements FrequencerInterface{
     }
 }
 */
-//演習3&4
+
+//演習3&4にて追加したコード
 public class Frequencer implements FrequencerInterface{
     // Code to start with: This code is not working, but good start point to work.
+    // Code with start：このコードは機能しませんが、良いスタートポイントがあります。
     byte [] myTarget;
     byte [] mySpace;
     boolean targetReady = false;
@@ -92,6 +95,9 @@ public class Frequencer implements FrequencerInterface{
     // The variable, "suffixArray" is the sorted array of all suffixes of mySpace.
     // Each suffix is expressed by a interger, which is the starting position in mySpace.
     // The following is the code to print the variable
+    // 変数 "suffixArray"は、mySpaceのすべての接尾辞のソートされた配列です。
+    // 各接尾辞はintergerで表され、これはmySpaceの開始位置です。
+    // 以下は変数を出力するコードです
     private void printSuffixArray() {
         if(spaceReady) {
             for(int i=0; i< mySpace.length; i++) {
@@ -104,6 +110,17 @@ public class Frequencer implements FrequencerInterface{
             }
         }
     }
+    
+    // suffixを表示
+    private void printMySpace(int num) {
+        int i = suffixArray[num];
+        System.out.print(num+":");
+        for(int j=i; j<mySpace.length; j++) {
+            System.out.write(mySpace[j]);
+        }
+        System.out.write('\n');
+    }
+    
     private int suffixCompare(int i, int j) {
         // comparing two suffixes by dictionary order.
         // i and j denoetes suffix_i, and suffix_j
@@ -117,7 +134,20 @@ public class Frequencer implements FrequencerInterface{
         // "Hi" < "Ho" ; if head is same, compare the next element
         // "Ho" < "Ho " ; if the prefix is identical, longer string is big
         
+        // 辞書順で2つの接尾辞を比較する。
+        // iとjはsuffix_iとsuffix_jをデノートします
+        // suffix_i > suffix_jの場合、1を返します
+        // suffix_i < suffix_jの場合、-1を返します。
+        // suffix_i = suffix_jの場合、0を返します。
+        // まだ実装されていませんが、
+        // 接尾辞配列の作成に使用する必要があります。
+        // 辞書順の例
+        // "i" <"o"：コードで比較する
+        // "Hi" <"Ho"; headが同じなら、次の要素を比較する
+        // "Ho" <"Ho"; 接頭辞が同一であれば、長い文字列が大きい
+        
         /*
+         //以下、途中で諦めたコード
          int si = suffixArray[i];
          int sj = suffixArray[j];
          
@@ -125,31 +155,30 @@ public class Frequencer implements FrequencerInterface{
          byte compare1[] = new byte[mySpace.length-si];
          int count = 0;
          for(int x = si;x<mySpace.length;x++){
-         compare1[count] = mySpace[x];
-         count++;
+            compare1[count] = mySpace[x];
+            count++;
          }
          
          byte compare2[] = new byte[mySpace.length-sj];
          count = 0;
          for(int y = sj;y<mySpace.length;y++){
-         compare2[count] = mySpace[y];
-         count++;
+            compare2[count] = mySpace[y];
+            count++;
          }
          
          count =0;
          //配列を比較する
          while(true){
-         if(compare1[count]==compare2[count]){
-         if(count+1 > compare1.length && count+1 > compare1.length){//1と2が同じなら0を返す
-         return  0;
-         }
-         }
-         else if(compare1[count]>compare2[count]){//iがjより大きかったら1を返す
-         return 1;
-         }else{//それ以外は-1を返す
-         return -1;
-         }
-         count ++;
+            if(compare1[count]==compare2[count]) {
+                if(count+1 > compare1.length && count+1 > compare1.length) { //1と2が同じなら0を返す
+                    return  0;
+                }
+            } else if(compare1[count]>compare2[count]) { //iがjより大きかったら1を返す
+                return 1;
+            } else {//それ以外は-1を返す
+                return -1;
+            }
+            count ++;
          }
          */
         
@@ -161,25 +190,177 @@ public class Frequencer implements FrequencerInterface{
         if(sj > s) s= sj;
         int n = mySpace.length - s;
         for(int k=0;k<n;k++) {
-            if(mySpace[si+k]>mySpace[sj+k]) return 1;
-            if(mySpace[si+k]<mySpace[sj+k]) return -1;
+            if(mySpace[si+k] > mySpace[sj+k]) return 1; //suffix_i > suffix_j
+            if(mySpace[si+k] < mySpace[sj+k]) return -1; //suffix_i < suffix_j
         }
         // 接頭辞が等しいので、文字列の長さ（インデックス）で大小比較
-        if(si < sj) return 1;
-        if(si > sj) return -1;
-        return 0;
+        if(si < sj) return 1; //suffix_i > suffix_j
+        if(si > sj) return -1; //suffix_i < suffix_j
+        return 0; //suffix_i = suffix_j
     }
     
+    /** クイックソート用 **/
+    /* mySpaceの中身を取得 */
+    private byte[] getMySpace(int num) {
+        // suffixArrayで指定したmySpaceから中身を取得し、配列に格納する
+        int i = suffixArray[num];
+        int k = 0;
+        byte[] contents = new byte[mySpace.length-i];
+        for(int j=i; j<mySpace.length; j++) {
+            contents[k] = mySpace[j];
+            k++;
+        }
+        return contents;
+    }
+    
+    /* ピボットとmySpaceを比較 */
+    private int suffixComparePivot(int i, byte []j) {
+        // suffixCompareを特定のsuffixと比較するように変更したもの
+        // 返り値はsuffixCompareと同じ
+        int si = suffixArray[i];
+        int sj = mySpace.length - j.length;
+        int s = 0;
+        if(si > s) s= si;
+        if(sj > s) s= sj;
+        int n = mySpace.length - s;
+        
+        for(int k=0;k<n;k++) {
+            if(mySpace[si+k] > j[k]) return 1; //suffix_i > suffix_j
+            if(mySpace[si+k] < j[k]) return -1; //suffix_i < suffix_j
+        }
+        // 接頭辞が等しいので、文字列の長さ（インデックス）で大小比較
+        if(si < sj) return 1; //suffix_i > suffix_j
+        if(si > sj) return -1; //suffix_i < suffix_j
+        return 0; //suffix_i = suffix_j
+    }
+    
+    /* ピボット選択 */
+    private int pivot(int i,int j){
+        // iからjの範囲で先頭と次の異なる値を比較し、大きい方を返す
+        int k = i+1;
+        while(k<=j && suffixCompare(i,k)==0) k++; //先頭と異なる値を探す
+        if(k > j) return -1; // iからjまでの範囲の値が全て同じ値だった場合
+        if(suffixCompare(i,k)==0 || suffixCompare(i,k)==1) { //suffix_i >= suffix_k の場合
+            return i;
+        } else {
+            return k;
+        }
+    }
+    
+    /* パーティション分割 */
+    private int partition(int i,int j,int x){
+        // iからjまでの範囲を軸要素xでソートし分割
+        int l=i;
+        int r=j;
+        byte []pivot = getMySpace(x); //軸要素の取得
+        
+        /*
+        //渡されたピボットを表示（デバッグ用）
+        printMySpace(x);
+        //取得したピボットの配列を表示（デバッグ用）
+        System.out.print("pivot[]:");
+        for(int poi=0; poi<pivot.length; poi++) {
+            System.out.write(pivot[poi]);
+        }
+        System.out.write('\n');
+        */
+        
+        // 検索が交差するまで繰り返す
+        while(l<=r){
+            
+            // 軸要素以上のデータを探す
+            while(l<=j && suffixComparePivot(l,pivot)==-1)  { //l<=j && suffix_r < suffix_x
+                l++;
+            }
+            
+            // 軸要素未満のデータを探す
+            while(r>=i && ( suffixComparePivot(r,pivot)==1 || suffixComparePivot(r,pivot)==0 )) { //r>=i && suffix_r >= suffix_x
+                r--;
+            }
+            
+            // 探索位置が交差しているので終了
+            if(l>r) break;
+            
+            // 値の入れ替え
+            int temp = suffixArray[l];
+            suffixArray[l] = suffixArray[r];
+            suffixArray[r] = temp;
+            l++;
+            r--;
+        }
+        //System.out.println("RETURN_l:"+l); //返り値を表示
+        return l;
+    }
+    
+    /* クイックソート */
+    private void quickSort(int i,int j){
+        if(i==j) return;
+        int p = pivot(i,j); //軸を求める
+        /*
+        // ピボットを表示
+        System.out.print("ピボット:");
+        printMySpace(p);
+        System.out.println();
+        */
+        if(p != -1){
+            int k = partition(i,j,p); //分割する
+            /*
+            // パーティション結果を表示
+            System.out.print("パーティション:"); //パーティションを表示
+            printMySpace(k);
+            System.out.println("パーティション後のarray");
+            printSuffixArray();
+            */
+            //分割された左側と右側を再帰的にソートする
+            quickSort(i,k-1); //左側
+            quickSort(k,j); //右側
+        }
+    }
+    /** クイックソート終わり **/
+
     public void setSpace(byte []space) {
+        spaceReady = false; //フラグの初期化
         mySpace = space;
-        if(mySpace.length>0) spaceReady = true;
+        if(mySpace.length > 0) {
+            spaceReady = true;
+        } else { //Spaceの長さが0 or 設定されていない場合は何もせずに終わる
+            return;
+        }
+        
+        /* デバッグ用 */
+        //System.out.println("spaceReady = "+spaceReady); //フラグの確認
+        //System.out.println("mySpace.length = "+mySpace.length); //判定条件に用いられる値を確認
+        
         suffixArray = new int[space.length];
         // put all suffixes in suffixArray. Each suffix is expressed by one interger.
+        // すべての接尾辞を接尾辞Arrayに入れます。 各接尾辞は整数で表されます。
         for(int k = 0; k< space.length; k++) {
             suffixArray[k] = k;
         }
-        // Sorting is not implmented yet.
-        // ここでバブルソートを実行
+        
+        /*
+        // ソート前のsuffixarrayを表示
+        System.out.println("suffixarray is not sorting");
+        printSuffixArray(); //suffixarrayの表示
+        */
+        
+        /*
+        // 比較用（デバッグ用）
+        int test1 = 3; //比較するsuffix
+        int test2 = 1; //比較するsuffix
+        int test = suffixCompare(test1,test2);
+        System.out.println();
+        printMySpace(test1);
+        printMySpace(test2);
+        System.out.println("比較結果:"+test);
+        System.out.println();
+        */
+        
+        // クイックソートを実行
+        quickSort(0,suffixArray.length-1);
+        
+        /*
+        // バブルソートを実行
         for(int i = 0; i< space.length; i++){
             for(int j = i+1; j < space.length; j++){
                 int tmp;
@@ -191,8 +372,13 @@ public class Frequencer implements FrequencerInterface{
                 }
             }
         }
+        */
         
-        printSuffixArray();
+        /*
+        // ソート後のsuffixarrayを表示
+        System.out.println("suffixarray is sorting");
+        printSuffixArray(); //suffixarrayの表示
+        */
         
         /* Example from "Hi Ho Hi Ho"
          0: Hi Ho
@@ -298,30 +484,51 @@ public class Frequencer implements FrequencerInterface{
     //検索に引っかかった要素の数を返す
     public int subByteFrequency(int start, int end) {
         //This method could be defined as follows though it is slow.
+        //このメソッドは遅いですが、以下のように定義することができます。
         int spaceLength = mySpace.length;
         int count = 0;
-        for(int offset = 0; offset< spaceLength - (end - start); offset++) {
+        for(int offset = 0; offset < spaceLength - (end - start); offset++) {
             boolean abort = false;
-            for(int i = 0; i< (end - start); i++) {
-                if(myTarget[start+i] != mySpace[offset+i]) { abort = true; break; }
+            for(int i = 0; i < (end - start); i++) { //ターゲットの長さ分だけ繰り返す
+                if(myTarget[start+i] != mySpace[offset+i]) { //ターゲットと文字列の比較
+                    abort = true;
+                    break;
+                }
             }
             if(abort == false) {
                 count++;
+                offset += (end - start) -1;
             }
         }
+        
         int first = subByteStartIndex(start,end);
         int last1 = subByteEndIndex(start, end);
+        
+        /*
         //inspection code
+        //検査コード
         for(int k=start;k<end;k++) {
             System.out.write(myTarget[k]);
         }
         System.out.printf(": first=%d last1=%d\n", first, last1);
-        return last1 - first;
+        */
+        
+        if( count < (last1 - first)){
+            return count;
+        } else {
+            return last1 - first;
+        }
+        
     }
     
     public void setTarget(byte [] target) {
+        targetReady = false; //フラグを初期化
         myTarget = target;
-        if(myTarget.length>0) targetReady = true;
+        if(myTarget.length > 0) targetReady = true;
+        
+        /* デバック用 */
+        //System.out.println("targetReady = "+targetReady); //フラグの確認
+        //System.out.println("myTarget.length = "+myTarget.length); //判定条件に用いられる値を確認
     }
     
     //条件がそろっていたらオブジェクトを作成
--- s4/b173311/InformationEstimator.java
+++ s4/b173311/InformationEstimator.java
@@ -17,33 +17,84 @@ public interface InformationEstimatorInterface{
 
 public class InformationEstimator implements InformationEstimatorInterface{
     // Code to tet, *warning: This code condtains intentional problem*
+    boolean targetReady = false;
+    boolean spaceReady = false;
     byte [] myTarget; // data to compute its information quantity
     byte [] mySpace;  // Sample space to compute the probability
     FrequencerInterface myFrequencer;  // Object for counting frequency
-
+    
+    /*
     byte [] subBytes(byte [] x, int start, int end) {
         // corresponding to substring of String for  byte[] ,
         // It is not implement in class library because internal structure of byte[] requires copy.
+        // byte []のStringの部分文字列に対応し、
+        // byte []の内部構造がコピーを必要とするため、クラスライブラリに実装されていません。
         byte [] result = new byte[end - start];
         for(int i = 0; i<end - start; i++) { result[i] = x[start + i]; };
         return result;
     }
+    */
 
     // IQ: information quantity for a count,  -log2(count/sizeof(space))
+    // IQ：カウントの情報量、-log2（count / sizeof（space））
     double iq(int freq) {
         return  - Math.log10((double) freq / (double) mySpace.length)/ Math.log10((double) 2.0);
     }
 
     public void setTarget(byte [] target) {
         myTarget = target;
+        if(target.length>0) targetReady = true;
     }
     public void setSpace(byte []space) { 
         myFrequencer = new Frequencer();
         mySpace = space;
         myFrequencer.setSpace(space);
+        spaceReady = true;
     }
 
     public double estimation(){
+        /* 解答 */
+        if(targetReady == false) return (double) 0.0;
+        if(spaceReady == false) return Double.MAX_VALUE;
+        
+        myFrequencer.setTarget(myTarget);
+        
+        double [] prefixEstimation = new double[myTarget.length+1];
+        
+        prefixEstimation[0] = (double) 0.0; //IE("") = 0.0;
+        
+        for(int n=1;n<=myTarget.length;n++) {
+            // target = "abcdef..", n = 4 for example, subByte(0, 4) = "abcd",
+            // IE("abcd") = min( IE("")+iq(#"abcd"),
+            //                   IE("a") + iq(#"bcd"),
+            //                   IE("ab")+iq(#"cd"),
+            //                   IE("abc")+iq(#"d") )
+            // prefixEstimation[0] = IE(""), subByte(0,4) = "abcd",
+            // prefixEstimation[1] = IE("a");  subByte(1,4)= "bcd",
+            // prefixEstimation[2] = IE("ab");  subByte(2,4)= "cd",
+            // prefixEstimation[3] = IE("abc");  subByte(3,4)= "d",
+            // prefixEstimation[4] = IE("abcd");
+            //
+            double value = Double.MAX_VALUE;
+            for(int start=n-1;start>=0;start--) {
+                int freq = myFrequencer.subByteFrequency(start, n);
+                if(freq != 0) {
+                    // update "value" if it is needed.
+                    double value1 = prefixEstimation[start]+iq(freq);
+                    if(value>value1) value = value1;
+                } else {
+                    // here freq ==0. This means iq(freq) is infinite.
+                    // freq is monotonically descreasing in this loop.
+                    // Now the current "value" is the minimum.
+                    break; 
+                }
+            }
+            prefixEstimation[n]=value;
+        }
+        return prefixEstimation[myTarget.length];
+        
+        /*
+        //以下、自分の元のやつ
         // TAGETが設定されていない場合
         if(myTarget == null) {
             return 0.0;
@@ -54,6 +105,7 @@ public class InformationEstimator implements InformationEstimatorInterface{
         np = 1<<(myTarget.length-1);
         // System.out.println("np="+np+" length="+myTarget.length);
         double value = Double.MAX_VALUE; // value = mininimum of each "value1".
+        // value =各 "value1"の最小値。
         
         // SPACEが設定されいない場合
         if(mySpace == null) {
@@ -72,6 +124,10 @@ public class InformationEstimator implements InformationEstimatorInterface{
             // for partition {"ab" "cde" "fg"}
             // a b c d e f g   : myTarget
             // T F T F F T F T : partition:
+            // pのバイナリ表現はパーティションを形成します。
+            //パーティションのために {"ab" "cde" "fg"}
+            // a b c d e f g   : myTarget
+            // T F T F F T F T : partition:
             partition[0] = true; // I know that this is not needed, but..
             for(int i=0; i < myTarget.length -1;i++) {
                 partition[i+1] = (0 !=((1<<i) & p));
@@ -80,6 +136,8 @@ public class InformationEstimator implements InformationEstimatorInterface{
 
             // Compute Information Quantity for the partition, in "value1"
             // value1 = IQ(#"ab")+IQ(#"cde")+IQ(#"fg") for the above example
+            // "value1"にあるパーティションの情報量を計算する
+            // 上記の例の場合、value1 = IQ（＃ "ab"）+ IQ（＃ "cde"）+ IQ（＃ "fg"）
             double value1 = (double) 0.0;
             int end = 0;;
             int start = end;
@@ -101,6 +159,7 @@ public class InformationEstimator implements InformationEstimatorInterface{
             if(value1 < value) value = value1;
         }
         return value;
+        */
     }
 
     public static void main(String[] args) {
